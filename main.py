f125af336fcddd60252402a3be9104df36ef3ca3
Whoami8587 <alita1387sobhan1385@gmail.com>
2025-07-15 19:04:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 from aiogram.fsm.storage.memory import MemoryStorage
+from config import TELEGRAM_BOT_TOKEN
 from bot.handlers import router as handlers_router
 from bot.payments import router as payments_router
-from config import TELEGRAM_BOT_TOKEN
-
-# ساخت نمونه Bot و Dispatcher
-bot = Bot(token=TELEGRAM_BOT_TOKEN)
-dp = Dispatcher(storage=MemoryStorage())
 
 async def main():
-    # اضافه کردن تمام روت‌های هندلر به Dispatcher
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+
     dp.include_router(handlers_router)
     dp.include_router(payments_router)
 
-    print("🤖 ربات در حال اجراست...")
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
