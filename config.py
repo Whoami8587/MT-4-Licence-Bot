038b94d4541c18f61729a17f3c910bfb14dca671
Whoami8587 <alita1387sobhan1385@gmail.com>
2025-07-15 19:07:23 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-# بارگذاری متغیرهای محیطی از فایل .env
 load_dotenv()
 
-# خواندن مقادیر مورد نیاز
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 ZARINPAL_GATEWAY_URL = os.getenv("ZARINPAL_GATEWAY_URL")
 DATABASE_URL = os.getenv("DATABASE_URL")
